9755c94969b630444fb582c05880ee3e0b921dce
Andriy Barskyi <barskyiandriy@gmail.com>
2022-07-11 14:29:18 +0300

    Add existing project files to Git

---
+++ src/main/java/com/example/sporthubujp/SportHubUjpApplication.java
@@ -0,0 +1,13 @@
+package com.example.sporthubujp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportHubUjpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportHubUjpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sporthubujp/SportHubUjpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sporthubujp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportHubUjpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
