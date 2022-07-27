5f50be19eacdde326476b4391915b7facf04d731
Ostap Klymko <oostapk@gmail.com>
2022-07-27 16:05:21 +0300

    added .env file to .gitignore and changed liquibase.properties back to initial ones

--- src/main/java/com/softserve/edu/sporthubujp/controllers/ArticleController.java
+++ src/main/java/com/softserve/edu/sporthubujp/controllers/ArticleController.java
@@ -5,10 +5,12 @@ import com.softserve.edu.sporthubujp.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/articles")
+
 public class ArticleController {
     private final ArticleService articleService;
 
@@ -18,6 +20,7 @@ public class ArticleController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_AUTHORIZED_USER')")
     public ResponseEntity<ArticleDTO> getArticleById(@PathVariable String id) {
         return ResponseEntity.status(HttpStatus.OK).body(
                 articleService.getArticleById(id));
