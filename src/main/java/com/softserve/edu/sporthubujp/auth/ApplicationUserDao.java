b6dfa3284d06ef10aaa5304c34ea9674965a014c
Ostap Klymko <oostapk@gmail.com>
2022-07-27 17:54:08 +0300

    created ApplicationUserDao interface and ApplicationUSerService class to implement UserDetailsService

---
+++ src/main/java/com/softserve/edu/sporthubujp/auth/ApplicationUserDao.java
@@ -0,0 +1,7 @@
+package com.softserve.edu.sporthubujp.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+}
