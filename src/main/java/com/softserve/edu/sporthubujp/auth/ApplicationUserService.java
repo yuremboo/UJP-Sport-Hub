ab90e5f96961d8a1adcdaa1803e3a5375460b7b2
Ostap Klymko <oostapk@gmail.com>
2022-07-27 18:09:53 +0300

    added one more configure method implementation and daoAuthenticationProvider methods to AppSecurityConfig

--- src/main/java/com/softserve/edu/sporthubujp/auth/ApplicationUserService.java
+++ src/main/java/com/softserve/edu/sporthubujp/auth/ApplicationUserService.java
@@ -1,6 +1,7 @@
 package com.softserve.edu.sporthubujp.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,7 +13,7 @@ public class ApplicationUserService implements UserDetailsService {
     private final ApplicationUserDao applicationUserDao;
 
     @Autowired
-    public ApplicationUserService(ApplicationUserDao applicationUserDao) {
+    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
         this.applicationUserDao = applicationUserDao;
     }
 
