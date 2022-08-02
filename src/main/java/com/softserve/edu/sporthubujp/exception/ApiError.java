127c9a88ec6f23f5301f61ec9ca3a67d14a71eb5
Andriy Barskyi <barskyiandriy@gmail.com>
2022-08-02 16:24:45 +0300

    implement test version of CustomRestExceptionHandler

--- src/main/java/com/softserve/edu/sporthubujp/exception/ApiError.java
+++ src/main/java/com/softserve/edu/sporthubujp/exception/ApiError.java
@@ -1,27 +1,43 @@
 package com.softserve.edu.sporthubujp.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
-public class ApiError {
+@Data
+class ApiError {
 
     private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
     private String message;
-    private List<String> errors;
+    private String debugMessage;
+    private List<ApiSubError> subErrors;
 
-    public ApiError(HttpStatus status, String message, List<String> errors) {
-        super();
-        this.status = status;
-        this.message = message;
-        this.errors = errors;
+    private ApiError() {
+        timestamp = LocalDateTime.now();
     }
 
-    public ApiError(HttpStatus status, String message, String error) {
-        super();
+    ApiError(HttpStatus status) {
+        this();
+        this.status = status;
+    }
+
+    ApiError(HttpStatus status, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = "Unexpected error";
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+
+    ApiError(HttpStatus status, String message, Throwable ex) {
+        this();
         this.status = status;
         this.message = message;
-        errors = Arrays.asList(error);
+        this.debugMessage = ex.getLocalizedMessage();
     }
 }
--- src/main/java/com/softserve/edu/sporthubujp/exception/CustomExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.edu.sporthubujp.exception;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
-
-}
