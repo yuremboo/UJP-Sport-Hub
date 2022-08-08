a94e7b79e50969003c39e50a495d7c8eccc4a530
Ostap Klymko <oostapk@gmail.com>
2022-08-09 00:39:25 +0300

    overrode EmailValidator test method for email validation

--- src/main/java/com/softserve/edu/sporthubujp/email/EmailValidator.java
+++ src/main/java/com/softserve/edu/sporthubujp/email/EmailValidator.java
@@ -3,12 +3,19 @@ package com.softserve.edu.sporthubujp.email;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
+
+    String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+" +
+                    "/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+    Pattern pattern = Pattern.compile(regex);
+
     @Override
-    public boolean test(String s) {
-        //TODO: Regex to validate email
-        return true;
+    public boolean test(String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
