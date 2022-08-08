4ca5f3b07e80138b6cdfdd878ad0a2f5e3777561
Oksana-Olesja <oksanavilchinska2003@gmail.com>
2022-08-08 21:15:08 +0300

    add CommentService in ArticleController and add method (getAllCommentByArticleId)

--- src/main/java/com/softserve/edu/sporthubujp/controller/ArticleController.java
+++ src/main/java/com/softserve/edu/sporthubujp/controller/ArticleController.java
@@ -1,11 +1,15 @@
 package com.softserve.edu.sporthubujp.controller;
 
+import java.util.List;
+
 import com.softserve.edu.sporthubujp.dto.ArticleDTO;
+import com.softserve.edu.sporthubujp.dto.CommentDTO;
 import com.softserve.edu.sporthubujp.service.ArticleService;
+import com.softserve.edu.sporthubujp.service.CommentService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,16 +17,23 @@ import org.springframework.web.bind.annotation.*;
 
 public class ArticleController {
     private final ArticleService articleService;
+    private final CommentService commentService;
 
     @Autowired
-    public ArticleController(ArticleService articleService) {
+    public ArticleController(ArticleService articleService, CommentService commentService) {
         this.articleService = articleService;
+        this.commentService = commentService;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ArticleDTO> getArticleById(@PathVariable String id) {
         return ResponseEntity.status(HttpStatus.OK).body(
                 articleService.getArticleById(id));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<List<CommentDTO>> getAllCommentByArticleId(@PathVariable String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            commentService.getAllCommentByArticleId(id));
     }
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteArticle(@PathVariable("id") String articleId)
