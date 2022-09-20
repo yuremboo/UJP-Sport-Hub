36f3780787b969abe278a677a81495a3ba9296b7
Ostap Klymko <oostapk@gmail.com>
2022-09-20 21:34:50 +0300

    found a way to get public ip

--- src/main/java/com/softserve/edu/sporthubujp/service/impl/GeolocationService.java
+++ src/main/java/com/softserve/edu/sporthubujp/service/impl/GeolocationService.java
@@ -9,9 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.URL;
 
 @Service
 @Slf4j
@@ -27,7 +30,14 @@ public class GeolocationService {
     public GeolocationResponseDTO getLocation(String ip)
             throws IOException, GeoIp2Exception {
         log.info(String.format("Service: posting geolocation by ip of %s ", ip));
+//
+        URL whatismyip = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                whatismyip.openStream()));
 
+        String extIp = in.readLine(); //you get the IP as a String
+        System.out.println(extIp);
+//
         InetAddress ipAddress = InetAddress.getByName(ip);
         CityResponse response = databaseReader.city(ipAddress);
         String cityName = response.getCity().getName();
