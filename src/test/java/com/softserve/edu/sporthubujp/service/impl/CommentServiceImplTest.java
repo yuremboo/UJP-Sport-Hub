790956632b420464ceaa15fab6580dbf18f0f849
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2022-09-20 17:26:16 +0300

    implement unit-test canAddNewComment

--- src/test/java/com/softserve/edu/sporthubujp/service/impl/CommentServiceImplTest.java
+++ src/test/java/com/softserve/edu/sporthubujp/service/impl/CommentServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.softserve.edu.sporthubujp.service.impl;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,7 @@ import com.softserve.edu.sporthubujp.dto.comment.CommentSaveDTO;
 import com.softserve.edu.sporthubujp.entity.Article;
 import com.softserve.edu.sporthubujp.entity.User;
 import com.softserve.edu.sporthubujp.entity.comment.Comment;
+import com.softserve.edu.sporthubujp.exception.InvalidEntityException;
 import com.softserve.edu.sporthubujp.mapper.CommentMapper;
 import com.softserve.edu.sporthubujp.repository.ArticleRepository;
 import com.softserve.edu.sporthubujp.repository.CommentRepository;
@@ -19,15 +22,19 @@ import com.softserve.edu.sporthubujp.repository.UserRepository;
 import com.softserve.edu.sporthubujp.service.CommentService;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceImplTest {
+
+    public static final String COMMENT_NOT_VALID_WITH = "Comment is not valid with %s";
     @Mock
     private CommentRepository commentRepository;
     @Mock
@@ -49,35 +56,40 @@ class CommentServiceImplTest {
     void updateComment() {
     }
 
-/*    @Test
-    void canAddNewComment() {
-        CommentSaveDTO commentDTO = spy(new CommentSaveDTO());
-        Comment comment = spy(new Comment());
-
-        given(commentRepository.save(comment));
-        when(commentMapper.dtoSaveToEntity(commentDTO))
-            .thenReturn(comment);
-
-
-        when(articleRepository.existsById(commentDTO.getArticleId())).thenReturn(true);
-        when(userRepository.existsById(commentDTO.getUserId())).thenReturn(true);
-
-        underTest.addNewComment(commentDTO);
-
-        ArgumentCaptor<Comment> commentArgumentCaptor =
-            ArgumentCaptor.forClass(Comment.class);
-
-        verify(commentRepository)
-            .save(commentArgumentCaptor.capture());
-
-        Comment capturedComment = commentArgumentCaptor.getValue();
-
-        assertThat(capturedComment).isEqualTo(comment);
-    }*/
-
     @Test
     void canAddNewComment() {
         CommentSaveDTO commentDTO = spy(new CommentSaveDTO());
         Comment comment = spy(new Comment());
+        when(commentMapper.dtoSaveToEntity(commentDTO))
+            .thenReturn(comment);
+        when(articleRepository.existsById(commentDTO.getArticleId()))
+            .thenReturn(true);
+        when(userRepository.existsById(commentDTO.getUserId()))
+            .thenReturn(true);
+        when(commentDTO.getLikes()).thenReturn(0);
+        when(commentDTO.getDislikes()).thenReturn(0);
+        underTest.addNewComment(commentDTO);
+        ArgumentCaptor<Comment> commentArgumentCaptor =
+            ArgumentCaptor.forClass(Comment.class);
+        verify(commentRepository).save(commentArgumentCaptor.capture());
+        Comment capturedComment = commentArgumentCaptor.getValue();
+        assertThat(capturedComment).isEqualTo(comment);
+    }
+
+    @Test
+    void cannotAddCommentWithNegativeNumberOfLikesOrDislikes() {
+        CommentSaveDTO commentDTO = spy(new CommentSaveDTO());
+        Comment comment = spy(new Comment());
+        when(commentMapper.dtoSaveToEntity(commentDTO))
+            .thenReturn(comment);
+        when(articleRepository.existsById(commentDTO.getArticleId()))
+            .thenReturn(true);
+        when(userRepository.existsById(commentDTO.getUserId()))
+            .thenReturn(true);
+        assertThatThrownBy(() -> underTest.addNewComment(commentDTO))
+            .isInstanceOf(InvalidEntityException.class)
+            .hasMessageContaining(String.format(COMMENT_NOT_VALID_WITH,
+                (commentDTO.getLikes() +
+                "likes and " + commentDTO.getDislikes() + " dislikes")));
     }
 }
